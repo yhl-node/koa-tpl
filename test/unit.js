f1da20f1db1bcb3e50e3b759f0171de7cf2e38f1
yhl <yhl@1024hw.cn>
2018-05-29 21:51:08 +0800

    Feature add ali docker (#4)
    * update
    
    * update
    
    * update

--- test/unit.js
+++ test/unit.js
@@ -2,11 +2,17 @@
  * @Author: yhl, yhl@1024hw.org
  * @Date: 2018-05-24 20:22:09
  * @Last Modified by: yhl
- * @Last Modified time: 2018-05-28 21:18:33
+ * @Last Modified time: 2018-05-29 21:44:39
  */
 
 test('1 + 1', () => {
   const response = 1 + 1
   expect(response).toBe(2)
   expect(response).toEqual(2)
+})
+
+test('1 - 1', () => {
+  const response = 1 - 1
+  expect(response).toBe(0)
+  expect(response).toEqual(0)
 })
