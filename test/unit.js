95c4ca76a4470f0da7a8c439dd90904c78222271
yeqingwen <317811448@qq.com>
2018-05-29 21:44:58 +0800

    update

--- test/unit.js
+++ test/unit.js
@@ -2,7 +2,7 @@
  * @Author: yhl, yhl@1024hw.org
  * @Date: 2018-05-24 20:22:09
  * @Last Modified by: yhl
- * @Last Modified time: 2018-05-24 20:26:01
+ * @Last Modified time: 2018-05-29 21:44:39
  */
 
 test('1 + 1', () => {
@@ -13,6 +13,6 @@ test('1 + 1', () => {
 
 test('1 - 1', () => {
   const response = 1 - 1
-  expect(response).toBe(2)
-  expect(response).toEqual(2)
+  expect(response).toBe(0)
+  expect(response).toEqual(0)
 })
