85eab762a06012a2008b245f05e87e91d08d291e
qingwen.ye <yhl@1024hw.cn>
2018-10-19 10:32:15 +0800

    add mysql tpl

---
+++ db/migrations/20181018063111-create-users.js
@@ -0,0 +1,36 @@
+'use strict'
+/**
+ * http://docs.sequelizejs.com/class/lib/query-interface.js~QueryInterface.html
+ */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users')
+  }
+}
