b3492f4e93ae46b3fd44c45afbc416b4f69bb317
yhl <yhl@1024hw.cn>
2018-11-23 16:20:25 +0800

    Feature add log (#14)
    * add log example
    
    * add jest config

--- src/middleware/verify-token.js
+++ src/middleware/verify-token.js
@@ -1,4 +1,4 @@
-
+import logger from '../logger'
 function verifyToken () {
   return (ctx, next) => {
     let token = ctx.request.query.token || 'guest'
@@ -6,6 +6,9 @@ function verifyToken () {
       ctx.state.verify = { status: true, len: token.length }
       return next()
     } else {
+      logger.info('auth error')
+      logger.error('auth error')
+      // ctx.throw(403, JSON.stringify({ code: 4003, msg: 'no auth' }))
       ctx.status = 403
       ctx.body = { code: 4003, msg: 'no auth' }
     }
