51aa191a38581c83c9ff5d1cc6f7790b963ad6e9
qingwen.ye <yhl@1024hw.cn>
2018-12-14 10:01:51 +0800

    describe

--- test/index.test.js
+++ test/index.test.js
@@ -11,47 +11,49 @@ test('echo status', async () => {
   expect(response.body).toEqual({ code: 0, status: 'ok' })
 })
 
-test('s1 service default', async () => {
-  const response = await supertest(app.callback()).get('/s1')
-  expect(response.status).toBe(200)
-  expect(response.body).toEqual({
-    code: 0,
-    msg: 'ok',
-    data: {
-      port: 3000,
-      info: { status: true, len: 5 }
-    }
+describe('s1 service', () => {
+  test('service default', async () => {
+    const response = await supertest(app.callback()).get('/s1')
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({
+      code: 0,
+      msg: 'ok',
+      data: {
+        port: 3000,
+        info: { status: true, len: 5 }
+      }
+    })
   })
-})
 
-test('s1 service ok', async () => {
-  const token = 'myselftoken'
-  const response = await supertest(app.callback()).get(`/s1?token=${token}`)
-  expect(response.status).toBe(200)
-  expect(response.body).toEqual({
-    code: 0,
-    msg: 'ok',
-    data: {
-      port: 3000,
-      info: { status: true, len: token.length }
-    }
+  test('service ok', async () => {
+    const token = 'myselftoken'
+    const response = await supertest(app.callback()).get(`/s1?token=${token}`)
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({
+      code: 0,
+      msg: 'ok',
+      data: {
+        port: 3000,
+        info: { status: true, len: token.length }
+      }
+    })
   })
-})
 
-test('s1 service 403', async () => {
-  const token = 'myt'
-  const response = await supertest(app.callback()).get(`/s1?token=${token}`)
-  expect(response.status).toBe(403)
-  expect(response.body).toEqual({
-    code: 4003,
-    msg: 'no auth'
+  test('s1 service 403', async () => {
+    const token = 'myt'
+    const response = await supertest(app.callback()).get(`/s1?token=${token}`)
+    expect(response.status).toBe(403)
+    expect(response.body).toEqual({
+      code: 4003,
+      msg: 'no auth'
+    })
   })
 })
 
 test('mockAxios', async () => {
   const token = 'myt'
   mockAxios.get.mockImplementationOnce(() =>
-    Promise.resolve({ data: {code: 0, msg: 'ok'} })
+    Promise.resolve({ data: { code: 0, msg: 'ok' } })
   )
   mockAxios.put.mockImplementationOnce(() =>
     Promise.resolve({ data: { code: 0, msg: 'ok' } })
