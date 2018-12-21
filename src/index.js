6efbf9d0dd5354d101448f30b3a4a879d2e70faf
qingwen.ye <yhl@1024hw.cn>
2018-12-21 17:43:00 +0800

    add websocket

--- src/index.js
+++ src/index.js
@@ -3,14 +3,26 @@ import config from 'config'
 import koaBody from 'koa-body'
 import logger from 'koa-logger'
 import cors from '@koa/cors'
+import WebSocket from 'ws'
+import http from 'http'
 import router from './router'
 
 const app = new Koa()
+
+const server = http.createServer(app.callback())
+const wss = new WebSocket.Server({ server })
 app.use(cors())
 app.use(koaBody())
 app.use(router.routes())
 app.use(logger())
 
-app.listen(config.server.port)
+wss.on('connection', (ws, req) => {
+  ws.on('message', (message) => {
+    console.log('received: %s', message)
+  })
+  ws.send('something')
+})
+
+server.listen(config.server.port)
 
 export default app
